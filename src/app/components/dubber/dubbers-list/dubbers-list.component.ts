68853f89944ad81fdd62c00aaddecf923f4827af
iladiro <dirosa.ilaria@gmail.com>
2017-12-12 22:39:53 +0100

    Add id property on currentDubber object

--- src/app/components/dubber/dubbers-list/dubbers-list.component.ts
+++ src/app/components/dubber/dubbers-list/dubbers-list.component.ts
@@ -18,30 +18,15 @@ export class DubbersListComponent implements OnInit {
 
   constructor(private dubberService: DubberService) {}
 
-  // currentItem.name:string;
-  //
-  // clear(){
-  //   this.currentItem.name = '';
-  // }
-
   onSubmit(form: NgForm){
     let currentDubber = form.value;
     let lengthDubbersIndex = Math.floor((Math.random() * 1000000) + 1);
-    let dubber:DubberModel{} = {
-      id: lengthDubbersIndex,
-      name: currentDubber.name,
-      surname: currentDubber.surname,
-      age: currentDubber.age,
-      gender: currentDubber.gender,
-      nationality: currentDubber.nationality,
-      photo: currentDubber.photo
-    };
+    currentDubber.id = lengthDubbersIndex;
     if(this.currentItem) {
       this.dubberService.updateDubber(this.currentItem);
       this.currentItem = undefined;
     } else {
-      //console.log("non ancora presente");
-      this.dubberService.addDubber(dubber);
+      this.dubberService.addDubber(currentDubber);
     }
   }
 
