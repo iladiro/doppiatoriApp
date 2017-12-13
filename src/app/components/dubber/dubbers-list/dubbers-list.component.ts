928227a8ab992f83292d1e32f20928258a4f7be6
iladiro <dirosa.ilaria@gmail.com>
2017-12-13 17:51:15 +0100

    clear code

--- src/app/components/dubber/dubbers-list/dubbers-list.component.ts
+++ src/app/components/dubber/dubbers-list/dubbers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DubberModel } from '../dubber-model';
 import { DubberService } from '../dubbers.service';
 import {NgForm} from '@angular/forms';
@@ -12,23 +12,9 @@ import {NgForm} from '@angular/forms';
 
 export class DubbersListComponent implements OnInit {
 
-  //@Input() dubbersList: DubberModel[];
-
   currentItem;
 
   constructor(private dubberService: DubberService) {}
-
-  // onSubmit(form: NgForm){
-  //   let currentDubber = form.value;
-  //   let lengthDubbersIndex = Math.floor((Math.random() * 1000000) + 1);
-  //   currentDubber.id = lengthDubbersIndex;
-  //   if(this.currentItem) {
-  //     this.dubberService.updateDubber(this.currentItem);
-  //     this.currentItem = undefined;
-  //   } else {
-  //     this.dubberService.addDubber(currentDubber);
-  //   }
-  // }
 
   onDelete(dubber) {
     this.dubberService.deleteDubber(dubber);
