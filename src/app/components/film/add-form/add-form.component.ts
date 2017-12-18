30e0038d77eed0f3192f56b5e36bc81a836f5f44
iladiro <dirosa.ilaria@gmail.com>
2017-12-18 18:39:00 +0100

    Add form to add film

---
+++ src/app/components/film/add-form/add-form.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FilmListComponent } from '../film-list/film-list.component';
+import { FilmModel } from '../film-model';
+import { FilmService } from '../film.service';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'addFilm',
+  templateUrl: './add-form.component.html',
+  styleUrls: ['./add-form.component.scss']
+})
+export class AddFormComponent implements OnInit {
+
+  @Input() film;
+
+  constructor(private filmService: FilmService) {}
+
+  onSubmit(form: NgForm){
+    let currentFilm = form.value;
+    let lengthFilmIndex = Math.floor((Math.random() * 1000000) + 1);
+    currentFilm.id = lengthFilmIndex;
+    if(this.film) {
+      this.filmService.updateFilm(this.film);
+      this.film = undefined;
+    } else {
+      this.filmService.addFilm(currentFilm);
+      form.reset();
+    }
+  }
+
+  ngOnInit() {}
+
+}
