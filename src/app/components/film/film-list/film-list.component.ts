8074bc71be3848072b82ccac9ea9ddbffd162765
iladiro <dirosa.ilaria@gmail.com>
2017-12-18 18:10:35 +0100

    Create table film list

--- src/app/components/film/film-list/film-list.component.ts
+++ src/app/components/film/film-list/film-list.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FilmModel } from '../film-model';
+import { FilmService } from '../film.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
-  selector: 'app-film-list',
+  selector: 'app-root',
   templateUrl: './film-list.component.html',
-  styleUrls: ['./film-list.component.scss']
+  styleUrls: ['./film-list.component.scss'],
+  providers: [FilmService]
 })
+
 export class FilmListComponent implements OnInit {
 
-  constructor() { }
+  currentItem;
+
+  constructor(private filmService: FilmService) {}
+
+  onDelete(film) {
+    this.filmService.deleteFilm(film);
+  }
+
+  getSingleItem(film) {
+    this.currentItem = film;
+  }
 
   ngOnInit() {
+    this.filmService.getFilms();
   }
 
 }
