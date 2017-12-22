e5211239bf10a8c4f650ced2fffc90d8c53ab624
iladiro <dirosa.ilaria@gmail.com>
2017-12-22 17:08:33 +0100

    Capitalize first letter on name and surname

--- src/app/components/dubber/add-dubber/add-dubber.component.ts
+++ src/app/components/dubber/add-dubber/add-dubber.component.ts
@@ -21,17 +21,18 @@ export class AddDubberComponent implements OnInit {
 
   constructor(private dubberService: DubberService) {}
 
-  getFirstChar() {
-    let createAvatar = this.currentDubber.name.charAt(0);
-    this.currentDubber.avatar = createAvatar;
+  getFirstChar(whichModel) {
+    let createAvatar = whichModel.name.charAt(0);
+    whichModel.avatar = createAvatar;
   }
 
   onSubmit(form: NgForm){
     this.currentDubber = form.value;
     let lengthDubbersIndex = Math.floor((Math.random() * 1000000) + 1);
     this.currentDubber.id = lengthDubbersIndex;
-    this.getFirstChar();
+    this.getFirstChar(this.currentDubber);
     if(this.dubber) {
+      this.getFirstChar(this.dubber);
       this.dubberService.updateDubber(this.dubber);
       this.dubber = undefined;
     } else {
