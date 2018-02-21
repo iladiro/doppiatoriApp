70ecc5572fe7e0d0101d5e13c7d6d617fbf282f2
iladiro <dirosa.ilaria@gmail.com>
2018-02-21 09:36:57 +0100

    ok

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -32,11 +32,11 @@ export class LoginComponent implements OnInit {
     this.loading = true;
     this.authenticationService.login(this.model.username, this.model.password).subscribe(
       data => {
-          this.router.navigate([this.returnUrl]);
+        this.router.navigate([this.returnUrl]);
       },
       error => {
-          this.alertService.error(error);
-          this.loading = false;
+        this.alertService.error(error);
+        this.loading = false;
       }
     );
   }
