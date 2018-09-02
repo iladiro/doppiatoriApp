76ae1a49681ced50a03c0fbf459086944fe2bb4c
iladiro <dirosa.ilaria@gmail.com>
2018-09-02 12:40:52 +0200

    ok

--- src/app/components/dubber/details/invoice/details/details.component.ts
+++ src/app/components/dubber/details/invoice/details/details.component.ts
@@ -20,12 +20,8 @@ import { Service } from '../../../../../services/index';
 })
 export class DetailsInvoiceComponent implements OnInit {
 
-  id: number;
-  private dubber: any;
-  private company: any;
-  private address: any;
-  private enpals_categories: any;
-  private invoice: any;
+  id:number;
+  private invoice: any = {};
 
   constructor(
     private service: Service,
@@ -40,7 +36,7 @@ export class DetailsInvoiceComponent implements OnInit {
       let img = canvas.toDataURL("image/png");
       let doc = new jsPDF('l', 'pt', 'a4');
       doc.addImage(img,'PNG',0,0);
-      doc.save(id  + ".pdf");
+      doc.save("fattura-" + id  + ".pdf");
     });
   }
 
@@ -49,27 +45,7 @@ export class DetailsInvoiceComponent implements OnInit {
       this.id = +params['id']; // (+) converts string 'id' to a number
       this.invoiceService.getById(this.id).subscribe(
         data => {
-          this.dubber = data.dubber;
-          this.enpals_categories = data.dubber.enpals_cat[0];
-          this.address = data.dubber.address[0];
-          this.company = data.company;
-          this.enpals_data = data.enpals_data;
-          this.film = data.film;
-          this.invoice = {
-            "reference_month": data.reference_month,
-            "reference_year": data.reference_year,
-            "work_to": data.work_to,
-            "work_from": data.work_from,
-            "number_of_days": data.number_of_days,
-            "creation_date": data.creation_date,
-            "amount": data.amount,
-            "total_amount": data.total_amount,
-            "vat": data.vat,
-            "trattenuta_sindacale": data.trattenuta_sindacale,
-            "total_deductions": data.total_deductions,
-            "total_enpals": data.total_enpals,
-            "total_net": data.total_net
-          }
+          this.invoice = data;
         }
       );
     });
