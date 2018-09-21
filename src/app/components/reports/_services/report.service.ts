17c808626266ed6f20e95867b946b5b407301516
iladiro <dirosa.ilaria@gmail.com>
2018-09-21 18:02:42 +0200

    show data

--- src/app/components/reports/_services/report.service.ts
+++ src/app/components/reports/_services/report.service.ts
@@ -8,31 +8,9 @@ export class ReportService {
   private url_root = "http://localhost:3000/";
 
   // Inject HttpClient into your component or service.
-  constructor(private http: HttpClient) {}
-
-  // getFilmsListFromYear(ref_year) {
-  //   return this.http.get(this.url_root + "?reference_year=eq." + ref_year + "&select=film_id, film_title");
-  // }
-  //
-  // getDubbersListFromYear(ref_year) {
-  //   return this.http.get(this.url_root + "?reference_year=eq." + ref_year + "&select=dubber_id, dubber_fullname");
-  // }
-  //
-  // getAllDubbersOfThatYear(ref_year) {
-  //   return this.http.get(this.url_root + "?reference_year=eq." + ref_year + "&select=dubber_id, dubber_fullname");
-  // }
-  //
-  // getDubberSelected(dubber_id, ref_year) {
-  //   return this.http.get(this.url_root + "?reference_year=eq." + ref_year + "&" + "dubber_id=eq." + dubber_id + "&select=dubber_id, dubber_fullname");
-  // }
-  //
-  // getCostFilm(film_id, ref_year) {
-  //   return this.http.get(this.url_root + "?film_id=eq." + film_id + "&reference_year=eq." + ref_year + "&select=amount,dubber_enpals_data:dubber_enpals_data(quota_enpals_ditta)");
-  // }
-  //
-  // getMonthlyCostFilm(ref_month, ref_year) {
-  //   return this.http.get(this.url_root + "?" + "reference_month=eq." + ref_month + "&reference_year=eq." + ref_year + "&select=title:film_title,amount_enpals:dubber_enpals_data(total_enpals)");
-  // }
+  constructor(
+    private http: HttpClient
+  ) {}
 
   getdata(table, query) {
     return this.http.get<any[]>(this.url_root + table + "?" + query);
